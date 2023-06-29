16c7b5734cbf0cdacebe384212062422d8367427
Neil <62321203+CestMerNeil@users.noreply.github.com>
2023-06-30 01:20:19 +0900

    Update ToLMDB.py

--- Neural_Network_Section/Caffe_NN/LeNet/Dataset/ToLMDB.py
+++ Neural_Network_Section/Caffe_NN/LeNet/Dataset/ToLMDB.py
@@ -5,23 +5,23 @@ from caffe.proto import caffe_pb2
 from PIL import Image
 
 
-def convert_to_lmdb(images_file, labels_file, output_dir):
-    # 读取MNIST数据集
+def convert_to_lmdb(images_file, labels_file, output_dir, lmdb_name):
+    # Read the MNIST dataset
     images = np.fromfile(images_file, dtype=np.uint8)[16:].reshape(-1, 784)
     labels = np.fromfile(labels_file, dtype=np.uint8)[8:]
 
-    # 创建LMDB环境
-    lmdb_path = os.path.join(output_dir, 'mnist_lmdb')
+    # Create LMDB environment
+    lmdb_path = os.path.join(output_dir, lmdb_name)
     env = lmdb.open(lmdb_path, map_size=int(1e9))
 
-    # 开始事务
+    # Start transaction
     with env.begin(write=True) as txn:
-        # 遍历数据并将其写入LMDB
+        # Iterate over the data and write it to LMDB
         for i in range(len(images)):
             image = images[i].reshape(28, 28)
             label = int(labels[i])
 
-            # 创建LMDB数据对象
+            # Create LMDB data object
             datum = caffe_pb2.Datum()
             datum.channels = 1
             datum.height = 28
@@ -29,32 +29,34 @@ def convert_to_lmdb(images_file, labels_file, output_dir):
             datum.label = label
             datum.data = image.tobytes()
 
-            # 序列化LMDB数据对象并写入事务
+            # Serialize the LMDB data object and write it to the transaction
             key = '{:08}'.format(i).encode('ascii')
             txn.put(key, datum.SerializeToString())
 
             if i % 1000 == 0:
-                print('已处理 {} / {} 个图像'.format(i, len(images)))
+                print('Processed {} / {} images'.format(i, len(images)))
 
-    # 关闭LMDB环境
+    # Close LMDB environment
     env.close()
 
-    print('转换完成！LMDB保存路径：{}'.format(lmdb_path))
+    print('Conversion completed! LMDB saved at: {}'.format(lmdb_path))
 
 
 if __name__ == '__main__':
-    current_dir = os.getcwd()  # 当前文件夹路径
+    current_dir = os.getcwd()  # Current folder path
 
-    # 训练集文件路径
+    # Training set file paths
     train_images_file = os.path.join(current_dir, 'train-images-idx3-ubyte')
     train_labels_file = os.path.join(current_dir, 'train-labels-idx1-ubyte')
 
-    # 测试集文件路径
+    # Test set file paths
     test_images_file = os.path.join(current_dir, 't10k-images-idx3-ubyte')
     test_labels_file = os.path.join(current_dir, 't10k-labels-idx1-ubyte')
 
-    # 转换训练集
-    convert_to_lmdb(train_images_file, train_labels_file, current_dir)
+    # Convert the training set to LMDB
+    train_lmdb_name = 'mnist_train_lmdb'
+    convert_to_lmdb(train_images_file, train_labels_file, current_dir, train_lmdb_name)
 
-    # 转换测试集
-    convert_to_lmdb(test_images_file, test_labels_file, current_dir)
+    # Convert the test set to LMDB
+    test_lmdb_name = 'mnist_test_lmdb'
+    convert_to_lmdb(test_images_file, test_labels_file, current_dir, test_lmdb_name)
