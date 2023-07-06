aa329b4727a7e3782bdc3973ebfb7f14191501cf
AO XIE DOCKER WASEDA <1453120442@qq.com>
2023-07-06 07:53:13 +0000

    test snnTorch

--- hls4ml/SNN/Extension_SNN_Leaky.py
+++ hls4ml/SNN/Extension_SNN_Leaky.py
@@ -1,6 +1,20 @@
-{
- "cells": [],
- "metadata": {},
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#!/usr/bin/python
+
+import torch
+
+class SNN_Leaky(tf.keras.layers.Layer):
+
+    def __init__(self):
+        super().__init__()
+
+    def call(self, inputs):
+        return tf.reverse(inputs, axis=[-1])
+
+class SNN_Leaky(hls4ml.model.layers.Layer):
+
+    def initialize(self):
+        inp = self.ger_input_variable()
+        shape = inp.shape
+        dims = inp.dim_names
+        self.add_output_variable(shape, dims)
+
